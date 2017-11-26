9eff1b55bbf0f0b5a0989baef3db129d69cbace3
Robert Chandler <robert@akchandler.com>
2017-11-25 21:44:11 -0700

    Restyle code for consistency

--- src/ImageGlitcher.java
+++ src/ImageGlitcher.java
@@ -28,13 +28,17 @@ public class ImageGlitcher {
 	 * @return an array of int rgb values
 	 */
 	public int[][] generateRGB(BufferedImage in) {
+		
 		int[][] rgb = new int[in.getHeight()][in.getWidth()];
 		for (int y = 0; y < in.getHeight(); y++) {
+			
 			for (int x = 0; x < in.getWidth(); x++) {
 				rgb[y][x] = in.getRGB(x, y);
 			}
+			
 		}
 		return rgb;
+		
 	}
 
 	/**
@@ -44,13 +48,19 @@ public class ImageGlitcher {
 	 * @return an array of int luminance values
 	 */
 	public int[][] generateLum(BufferedImage in) {
+		
 		int[][] lume = new int[in.getHeight()][in.getWidth()];
 		for (int y = 0; y < in.getHeight(); y++) {
+			
 			for (int x = 0; x < in.getWidth(); x++) {
-				lume[y][x] = (int) Math.sqrt((.299 * (in.getRGB(x, y) >> 16 & 0xFF)) + (.587 * (in.getRGB(x, y) >> 8 & 0xFF)) + (.114 * (in.getRGB(x, y) >> 0 & 0xFF)));
+				lume[y][x] = (int) Math.sqrt((.299 * (in.getRGB(x, y) >> 16 & 0xFF)) 
+						+ (.587 * (in.getRGB(x, y) >> 8 & 0xFF)) 
+						+ (.114 * (in.getRGB(x, y) >> 0 & 0xFF)));
 			}
+			
 		}
 		return lume;
+		
 	}
 
 	/**
@@ -60,13 +70,19 @@ public class ImageGlitcher {
 	 * @return an array of int luminance values
 	 */
 	public int[][] generateLum(int[][] rgb) {
+		
 		int[][] lume = new int[rgb.length][rgb[0].length];
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length; x++) {
-				lume[y][x] = (int) Math.sqrt((.299 * (rgb[y][x] >> 16 & 0xFF)) + (.587 * (rgb[y][x] >> 8 & 0xFF)) + (.114 * (rgb[y][x] >> 0 & 0xFF)));
+				lume[y][x] = (int) Math.sqrt((.299 * (rgb[y][x] >> 16 & 0xFF)) 
+						+ (.587 * (rgb[y][x] >> 8 & 0xFF)) 
+						+ (.114 * (rgb[y][x] >> 0 & 0xFF)));
 			}
+			
 		}
 		return lume;
+		
 	}
 
 	/**
@@ -75,13 +91,17 @@ public class ImageGlitcher {
 	 * @return an int array of rgb values inverse to the input
 	 */
 	public int[][] generateInverse(int[][] rgb) {
+		
 		int[][] inverse = new int[rgb.length][rgb[0].length];
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length; x++) {
 				inverse[y][x] = 0xFFFFFF - rgb[y][x];
 			}
+			
 		}
 		return inverse;
+		
 	}
 
 
@@ -91,13 +111,17 @@ public class ImageGlitcher {
 	 * @return an int array of greysclae rgb values.
 	 */
 	public int[][] generateRGBbyBrightness(int[][] lume) {
+		
 		int[][] rgb = new int[lume.length][lume[0].length];
 		for (int y = 0; y < lume.length; y++) {
+			
 			for (int x = 0; x < lume[0].length; x++) {
 				rgb [y][x] = (lume[y][x] * 0x110000) + (lume[y][x] * 0x1100) + (lume[y][x] * 0x11);
 			}
+			
 		}
 		return rgb;
+		
 	}
 
 	/**
@@ -107,6 +131,7 @@ public class ImageGlitcher {
 	 * @param lume - an int array of luminance values
 	 */
 	public void dependentRowSelectionSort(int[][] rgb, int[][] lume) {
+		
 		int min;
 		for (int y = 0; y < rgb.length; y++) {
 			for (int x = 0; x < rgb[0].length - 1; x++) {
@@ -117,6 +142,7 @@ public class ImageGlitcher {
 				doubleExchange(rgb[y], lume[y], x, min);
 			}
 		}
+		
 	}
 
 	/**
@@ -126,21 +152,30 @@ public class ImageGlitcher {
 	 * @param lume - an int array of luminance values
 	 */
 	public void dependentColSelectionSort(int[][] rgb, int[][] lume) {
+		
 		int[][] intermediateRGB = new int[rgb[0].length][rgb.length];
 		int[][] intermediateLum = new int[rgb[0].length][rgb.length];
+		
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length - 1; x++) {
 				intermediateRGB[x][y] = rgb[y][x];
 				intermediateLum[x][y] = lume[y][x];
 			}
+			
 		}
+		
 		this.dependentRowSelectionSort(intermediateRGB, intermediateLum);
+		
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length - 1; x++) {
 				rgb[y][x] = intermediateRGB[x][y];
 				lume[y][x] = intermediateLum[x][y];
 			}
+			
 		}
+		
 	}
 
 	/**
@@ -150,9 +185,11 @@ public class ImageGlitcher {
 	 * @param lume - an int array of luminance values
 	 */
 	public void dependentRowQuicksort(int[][] rgb, int[][]lume) {
+		
 		for (int y = 0; y < rgb.length; y++) {
 			dependentQuicksort(rgb[y], lume[y], 0, rgb[y].length - 1);
 		}
+		
 	}
 
 	/**
@@ -162,21 +199,30 @@ public class ImageGlitcher {
 	 * @param lume - an int array of luminance values
 	 */
 	public void dependentColQuicksort(int[][] rgb, int[][] lume) {
+		
 		int[][] intermediateRGB = new int[rgb[0].length][rgb.length];
 		int[][] intermediateLum = new int[rgb[0].length][rgb.length];
+		
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length - 1; x++) {
 				intermediateRGB[x][y] = rgb[y][x];
 				intermediateLum[x][y] = lume[y][x];
 			}
+			
 		}
+		
 		this.dependentRowQuicksort(intermediateRGB, intermediateLum);
+		
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length - 1; x++) {
 				rgb[y][x] = intermediateRGB[x][y];
 				lume[y][x] = intermediateLum[x][y];
 			}
+			
 		}
+		
 	}
 
 	/**
@@ -185,10 +231,13 @@ public class ImageGlitcher {
 	 * @param b
 	 */
 	private static void dependentQuicksort(int[] a, int[] b, int low, int high) {
+		
 		if (high <= low) return;
+		
 		int j = partition(a, b, low, high);
 		dependentQuicksort(a, b, low, j-1);
 		dependentQuicksort(a, b, j+1, high);
+		
 	}
 
 	/**
@@ -200,16 +249,20 @@ public class ImageGlitcher {
 	 * @return partition value
 	 */
 	private static int partition(int[] a, int[] b, int low, int high) {
+		
 		int i = low, j = high+1;
 		int v = b[low];
 		while (true) {
+			
 			while (b[++i] < v) if (i == high) break;
 			while (v < b[--j]) if (j == low) break;
 			if (i >= j) break;
 			doubleExchange(a, b, i, j);
+			
 		}
 		doubleExchange(a, b, low, j);
 		return j;
+		
 	}
 
 	/**
@@ -220,25 +273,44 @@ public class ImageGlitcher {
 	 * @param j - an index to be exchanged
 	 */
 	private static void doubleExchange(int[] a, int[] b, int i, int j) {
+		
 		int temp = b[i];
 		b[i] = b[j];
 		b[j] = temp;
+		
 		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
+		
 	}
 
 	/**
-	 * replaces each rgb value of in with the corresponding calue from rgb, assuming their dimensions match.
+	 * replaces each rgb value of in with the corresponding value from rgb, assuming their dimensions match.
 	 * @param in
 	 * @param rgb
 	 */
 	public void rewriteImg(BufferedImage in, int[][] rgb) {
+		
 		for (int y = 0; y < in.getHeight(); y++) {
+			
 			for (int x = 0; x < in.getWidth(); x++) {
 				in.setRGB(x, y, rgb[y][x]);
 			}
+			
 		}
+		
+	}
+	
+	/**
+	 * generates a white-on-black int array that represents the canny edge detection of the input.
+	 * @param in - 2d int array to process
+	 * @return edges - 2d int array edge detection
+	 */
+	public int[][] generateEdgeDetection(int[][] in) {
+		
+		CannyEdgeDetection edge = new CannyEdgeDetection(in);
+		return edge.toArray();
+		
 	}
 
 	/**
@@ -248,12 +320,14 @@ public class ImageGlitcher {
 	 * @return a BufferedImage object
 	 */
 	public BufferedImage generateRandomSubImg(BufferedImage in) {
+		
 		int x = (int) (rand.nextDouble() * (7 * (in.getWidth() / 8))) + (in.getWidth() / 16);
 		int y = (int) (rand.nextDouble() * (7 * (in.getHeight() / 8))) + (in.getHeight() / 16);
 		int w = (int) (rand.nextDouble() * (in.getWidth() - x));
 		int h = (int) Math.min(((rand.nextDouble() * (in.getHeight() - y)) / 2) + 1, (in.getHeight() / 16));
 		BufferedImage sub = in.getSubimage(x, y, w, h);
 		return sub;
+		
 	}
 
 	/**
@@ -267,11 +341,13 @@ public class ImageGlitcher {
 		int temp;
 
 		for (int y = 0; y < rh / 2; y++) {
+			
 			for (int x = 0; x < rw; x++) { 
 				temp = in.getRGB(x, y);
 				in.setRGB(x, y,  in.getRGB((rw - 1) - x, (rh - 1) - y));
 				in.setRGB((rw - 1) - x, (rh - 1) - y, temp);
 			}
+			
 		}
 
 	}
@@ -281,6 +357,7 @@ public class ImageGlitcher {
 	 * @param rgb - an array of rgb values
 	 */
 	public int[][] blockFlip(int[][] a) {
+		
 		int temp;
 		int[][] rgb = a;
 		int rw = a[0].length;
@@ -292,9 +369,11 @@ public class ImageGlitcher {
 				temp = rgb[y][x];
 				rgb[y][x] = rgb[(rh - 1) - y][(rw - 1) - x];
 				rgb[(rh - 1) - y][(rw - 1) - x] = temp;
+				
 			}
 		}
 		return rgb;
+		
 	}
 
 	/**
@@ -305,9 +384,12 @@ public class ImageGlitcher {
 	 * @throws IndexOutOfBoundsException
 	 */
 	public void translateX(int[][] a, int distance, int edgeMode) throws IndexOutOfBoundsException {
+		
 		int[][] orig =  a.clone();
+		
 		switch (edgeMode) {
-		case 0: // pad duplicates the leftmost row in the newly vacated space
+		
+		case EDGE_PAD: // pad duplicates the leftmost row in the newly vacated space
 			for (int y = 0; y < a.length; y++) {
 				for (int x = a[y].length - 1; x >= distance; x--) {
 					a[y][x] = orig[y][x - distance];
@@ -317,7 +399,8 @@ public class ImageGlitcher {
 				}
 			}
 			break;
-		case 1: // reflect reflects the leftmost columns across the transform line		
+			
+		case EDGE_REFLECT: // reflect reflects the leftmost columns across the transform line		
 			for (int y = 0; y < a.length; y++) {
 				for (int x = a[y].length - 1; x >= distance; x--) {
 					a[y][x] = orig[y][x - distance];
@@ -327,7 +410,8 @@ public class ImageGlitcher {
 				}
 			}
 			break;
-		case 2: // wrap moves the cropped portion of the image to the newly vacated columns
+			
+		case EDGE_WRAP: // wrap moves the cropped portion of the image to the newly vacated columns
 			// TODO: Currently wraps twice. I assumed using array orig would fix this... it didn't.
 			for (int y = 0; y < a.length; y++) {
 				for (int x = 0; x < distance; x++) {
@@ -338,7 +422,9 @@ public class ImageGlitcher {
 				}
 			}
 			break;
+			
 		}
+		
 	}
 	
 	/**
@@ -347,16 +433,23 @@ public class ImageGlitcher {
 	 * @param maxDistance - the maximum number of pixels a row should be shifted.
 	 */
 	public void jitterX(int[][] a, int maxDistance) {
+		
 		int[][] orig = a.clone();
+		
 		for (int y = 0; y < a.length; y++) {
+			
 			int distance = (int) ((rand.nextDouble()) * maxDistance);
+			
 			for (int x = a[y].length - 1; x >= distance; x--) {
 				a[y][x] = orig[y][x - distance];
 			}
+			
 			for (int x = distance - 1; x >= 0; x--) {
 				a[y][x] = orig[y][distance + (distance - x)];
 			}
+			
 		}
+		
 	}
 	
 	/**
@@ -365,16 +458,23 @@ public class ImageGlitcher {
 	 * @param maxDistance - the maximum number of pixels a column should be shifted.
 	 */
 	public void jitterY(int[][] a, int maxDistance) {
+		
 		int[][] orig = a.clone();
+		
 		for (int x = 0; x < a[0].length; x++) {
+			
 			int distance = (int) ((rand.nextDouble()) * maxDistance);
+			
 			for (int y = a.length - 1; y >= distance; y--) {
 				a[y][x] = orig[y - distance][x];
 			}
+			
 			for (int y = distance - 1; y >= 0; y--) {
 				a[y][x] = orig[distance + (distance - y)][x];
 			}
+			
 		}
+		
 	}
 	
 	/**
@@ -384,13 +484,18 @@ public class ImageGlitcher {
 	 * @return a color array (formatted like a lume array)
 	 */
 	public int[][] extractChannel(int[][] rgb, int channel) {
+		
 		int[][] color = new int[rgb.length][rgb[0].length];
+		
 		for (int y = 0; y < rgb.length; y++) {
+			
 			for (int x = 0; x < rgb[0].length; x++) {
 				color[y][x] = rgb[y][x] >> channel & 0xFF;
 			}
+			
 		}
 		return color;
+		
 	}
 	
 	/**
@@ -401,13 +506,18 @@ public class ImageGlitcher {
 	 * @return an rgb int array
 	 */
 	public int[][] blendChannels(int[][] r, int[][] g, int[][] b) {
+		
 		int[][] rgb = new int[r.length][r[0].length];
+		
 		for (int y = 0; y < r.length; y++) {
+			
 			for (int x = 0; x < r[0].length; x++) {
 				rgb [y][x] = (r[y][x] << 16) + (g[y][x] << 8) + (b[y][x]);
 			}
+			
 		}
 		return rgb;
+		
 	}
 
 	/**
@@ -416,13 +526,17 @@ public class ImageGlitcher {
 	 * @return a BufferedImage object loaded from input
 	 */
 	public BufferedImage load(String fileName) {
+		
 		BufferedImage in = null;
+		
 		try{
 			in = ImageIO.read(new File(fileName));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		return in;
+		
 	}
 
 	/**
@@ -431,6 +545,7 @@ public class ImageGlitcher {
 	 * @param fileName - string of a file name to use 
 	 */
 	public void save(BufferedImage in, String fileName) {
+		
 		JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
 		jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
 		jpegParams.setCompressionQuality(1f);
